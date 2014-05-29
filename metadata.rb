1ed3058a53b23768aa3cb6e50b5859301a1d6625
Konstantin Gontar <gontarkonstantin@gmail.com>
2014-05-29 11:07:17 +0300

    add coocbook author in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'core'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Konstantin Gontar'
+maintainer_email 'Konstantin.Gontar@toatech.com'
 license          'All rights reserved'
-description      'Installs/Configures core'
-long_description 'Installs/Configures core'
+description      'Installs/Configures core application'
+long_description 'Installs/Configures core application'
 version          '0.1.0'
 
 depends "freebsd"
